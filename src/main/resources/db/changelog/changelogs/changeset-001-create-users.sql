eaeda6927dd07ada01aadfbcbd449767f0550f71
Stefan.Golubov <stefan.golubov@qinshift.com>
2024-08-05 15:22:07 +0200

    Initial setup. Adding changesets

---
+++ src/main/resources/db/changelog/changelogs/changeset-001-create-users.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+
+--changeset Stefan:createUsersTable runAlways:true
+--comment: Creating Users table
+--preConditions onFail:MARK_RAN
+
+--sql
+CREATE TABLE IF NOT EXISTS public.Users (
+id UUID PRIMARY key default uuid_generate_v4() ,
+username VARCHAR(255) UNIQUE,
+password VARCHAR(255),
+email VARCHAR(255) UNIQUE,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP
+);
