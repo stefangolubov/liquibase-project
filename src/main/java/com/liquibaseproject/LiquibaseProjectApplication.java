72df1d3480add9614c4468b792055ecace6bf707
Stefan.Golubov <stefan.golubov@qinshift.com>
2024-08-08 15:37:08 +0200

    Adding SoapAPI configuration and api.yml (REST API methods and definition for Users)

--- src/main/java/com/liquibaseproject/LiquibaseProjectApplication.java
+++ src/main/java/com/liquibaseproject/LiquibaseProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.liquibaseproject;
+package com.liquibaseproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
